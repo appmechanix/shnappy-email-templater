d89f676fb5f9b77d7177eb9042b5d74cf22fd465
Daniel Wylie <daniel@danielwylie.me>
2016-12-06 15:18:12 +1300

    Add ability to load testing model

--- test/test.js
+++ test/test.js
@@ -1,15 +1,22 @@
 var templateEngine = require('../lib/index');
 
 exports.Test_That_Name_Gets_Replaces_When_Has_Model = function (test) {
-    templateEngine.RenderTemplate('test', { name: 'Daniel' }, function (renderedHTML) {
-        test.equal('Hey Daniel', renderedHTML);
-        test.done();
-    });
+	templateEngine.RenderTemplate('test', { name: 'Daniel' }, function (renderedHTML) {
+		test.equal('Hey Daniel', renderedHTML);
+		test.done();
+	});
 }
 
 exports.Test_That_Name_Is_Empty_When_Not_In_Model = function (test) {
-    templateEngine.RenderTemplate('test', {}, function (renderedHTML) {
-        test.equal('Hey ', renderedHTML);
-        test.done();
-    });
+	templateEngine.RenderTemplate('test', {}, function (renderedHTML) {
+		test.equal('Hey ', renderedHTML);
+		test.done();
+	});
+}
+
+exports.Test_That_Testing_Model_Is_Loaded = function(test){
+	templateEngine.RenderTemplate('test2', {isTest: true}, function (renderedHTML) {
+		test.equal('Hello Test', renderedHTML);
+		test.done();
+	});
 }
